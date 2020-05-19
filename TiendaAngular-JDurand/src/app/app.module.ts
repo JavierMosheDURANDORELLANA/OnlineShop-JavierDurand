2d384bb59897a156e759ebeffb206918ce0bca96
Javier Moshe Durand Orellana <javierdurand.webmeister@gmail.com>
2020-05-19 18:26:39 -0500

    Implementado el componente login

--- TiendaAngular-JDurand/src/app/app.module.ts
+++ TiendaAngular-JDurand/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+
   ],
   imports: [
     BrowserModule
